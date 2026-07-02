407c5952d16f1ef9aa546c731b99613c8c1d82e7
shrastichaudhry <shrastichaudhry737@gmail.com>
2026-07-02 11:30:59 +0530

    Fix middleware imports for deployment

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-const authMiddleware = require("../middleware/authmiddleware");
-const roleMiddleware = require("../middleware/rolemiddleware");
+const authMiddleware = require("../middleware/authMiddleware");
+const roleMiddleware = require("../middleware/roleMiddleware");
 
 const {
     createOrder,
--- backend/routes/salesRoutes.js
+++ backend/routes/salesRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const router = express.Router();
 
-const authMiddleware = require("../middleware/authmiddleware");
+const authMiddleware = require("../middleware/authMiddleware");
 const roleMiddleware = require("../middleware/roleMiddleware");
 
 const {
